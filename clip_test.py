2d4031234809306463261e9b7fafdc80de83989c
Michael Shahin <shahinmg@ku.edu>
2025-02-10 16:11:06 -0600

    local parquet

---
+++ clip_test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 10 15:58:38 2025
+
+@author: m484s199
+"""
+
+import os
+import geopandas as gpd
+
+all_bergs_path = '/media/m484s199/Sid_GDrive/FjordDistribution_GrIS/all_bergs_file/greenland_iceberg_dist_2017-2021.parquet'
+
+fjords_path = '/media/m484s199/Sid_GDrive/FjordDistribution_GrIS/separate_fjords/seperate_fjords.shp'
+
+# fjords_df = gpd.read_file(fjords_path, engine='pyogrio', use_arrow=True)
+bergs_df = gpd.read_file(all_bergs_path, engine='pyogrio', use_arrow=True)
