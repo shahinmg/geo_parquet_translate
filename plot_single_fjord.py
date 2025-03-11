a8e6ce0e945a69533255322758130cc2d6de4e17
shahinmg <shahinmg@ku.edu>
2025-03-11 11:14:56 -0500

    add some comments

--- plot_single_fjord.py
+++ plot_single_fjord.py
@@ -9,7 +9,7 @@ Created on Tue Mar 11 11:04:53 2025
 import geopandas as gpd
 import matplotlib.pyplot as plt
 
-#read file
+#read fjord geometry
 fjord_num = 127
 
 fjords_path = './separate_fjords/seperate_fjords.shp'
@@ -20,9 +20,10 @@ fjord_geom = fjords_df[msk].dissolve()
 fjord_geom_area = fjord_geom.area / 1e6
 fjord_geom_area = fjord_geom_area[0] 
 
-
+# read the iceberg time series data
 gdf = gpd.read_file('./dataframes/fjord_127.gpkg')
 
+# group by date and take sum of the iceberg area
 area_sum = gdf.groupby(['date'])[['AreaRF_km2']].sum()
 
 ax = area_sum.plot(title=f'Fjord {fjord_num}', ylabel=r'Iceberg Area (km$^{2}$)', 
